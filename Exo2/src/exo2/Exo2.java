293a3c33cda2854a40c5f0b935d5bb20954f10dc
marion dussart <mario@Ordinateur-Marion.education.lakanal.epf.fr>
2023-09-25 17:30:01 +0200

    Test d'envoie TP0

--- Exo2/src/exo2/Exo2.java
+++ Exo2/src/exo2/Exo2.java
@@ -15,6 +15,23 @@ public class Exo2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    public static void main(String[] args) {
+//Declaration des variables
+int nb; // nombre dentiers a additionner
+int result; // resultat
+int ind; //indice
+nb=5;
+result=0;
+// Addition des nb premiers entiers
+ind=1;
+while (ind <= nb) {
+result=result+ind;
+}
+// Affichage du resultat
+System.out.println();
+System.out.println("La somme des "+ nb + "entiers est: "+result);
+    
+    
     }
     
 }
