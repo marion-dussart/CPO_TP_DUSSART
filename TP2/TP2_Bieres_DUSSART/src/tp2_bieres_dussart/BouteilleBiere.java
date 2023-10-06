26573640590bae0bfc51152d78e776f804e024d4
marion dussart <mario@Ordinateur-Marion.education.lakanal.epf.fr>
2023-10-06 17:14:54 +0200

    Exercice 1 TP2

---
+++ TP2/TP2_Bieres_DUSSART/src/tp2_bieres_dussart/BouteilleBiere.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_dussart;
+
+/**
+ *
+ * @author mario
+ */
+public class BouteilleBiere {
+    String nom;
+    double degreAlcool;
+    String brasserie;
+    boolean ouverte;
+    
+public void lireEtiquette() {
+System.out.println("Bouteille de " + nom +" (" + degreAlcool +
+ " degres) \nBrasserie : " + brasserie ) ;
+}
+    
+public BouteilleBiere(String unNom, double unDegre, String
+uneBrasserie) {
+ nom = unNom;
+ degreAlcool = unDegre;
+ brasserie = uneBrasserie;
+ ouverte = false;
+}
+
+public boolean Decapsuler() {
+    if ( ouverte = false){
+        ouverte = true;
+    }
+    else {
+        System.out.println( "erreur : biere déjà ouverte");
+        ouverte = false;
+    }
+}
+
+public String toString() {
+String chaine_a_retourner;
+chaine_a_retourner = nom + " (" + degreAlcool + " degrés)
+Ouverte ? ";
+if (ouverte == true ) chaine_a_retourner += "oui" ;
+else chaine_a_retourner += "non" ;
+return chaine_a_retourner ;
+}
+}
