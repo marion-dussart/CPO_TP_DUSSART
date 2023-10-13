404e3c0d0fbc66fc63093d0ca5dbe08b20af57b6
marion dussart <mario@Ordinateur-Marion.education.lakanal.epf.fr>
2023-10-13 17:12:32 +0200

    Exercice 4 TP2 (manque la question 10)

---
+++ TP2/TP2_relation_1_DUSSART/src/tp2_relation_1_dussart/TP2_relation_1_DUSSART.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_relation_1_dussart;
+
+/**
+ *
+ * @author mario
+ */
+public class TP2_relation_1_DUSSART {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+Voiture uneClio = new Voiture ("Clio", "Renault", 5 ) ;
+Voiture uneAutreClio = new Voiture ("Clio", "Renault", 5 ) ;
+Voiture une2008 = new Voiture ("2008", "Peugeot", 6 ) ;
+Voiture uneMicra = new Voiture ("Micra", "Nissan", 4 ) ;
+Personne bob = new Personne("Bobby", "Sixkiller");
+Personne reno = new Personne("Reno", "Raines");
+System.out.println("liste des voitures disponibles "+ uneClio +
+"\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;
+        
+bob.liste_voitures[0] = uneClio ;
+bob.nbVoitures = 1 ;
+uneClio.proprietaire = bob ;    
+
+bob.liste_voitures[1] = une2008 ;
+bob.nbVoitures = 2 ;
+une2008.proprietaire = bob ;    
+
+
+
+reno.liste_voitures[0] = uneMicra ;
+reno.nbVoitures = 1 ;
+uneMicra.proprietaire = reno ;   
+
+reno.liste_voitures[1] = uneClio ;
+reno.nbVoitures = 1 ;
+uneClio.proprietaire = reno ;  
+
+System.out.println("la premiere voiture de Bob est " + bob.liste_voitures[0]
++ "la deuxieme voiture de Bob est " + bob.liste_voitures[1]  ) ;
+System.out.println("la premiere voiture de Bob est " + reno.liste_voitures[0]
++ "la deuxieme voiture de reno est " + reno.liste_voitures[1] ) ;
+
+
+    }
+    
+}
