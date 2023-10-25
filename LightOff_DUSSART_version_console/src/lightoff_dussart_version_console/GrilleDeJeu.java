90f32f8d98dce523f8def924d44a15579874b7f3
marion dussart <mario@Ordinateur-Marion.education.lakanal.epf.fr>
2023-10-25 16:46:04 +0200

    Les classes CelluleLumineuse et GrilleDeJeu sont fini

--- LightOff_DUSSART_version_console/src/lightoff_dussart_version_console/GrilleDeJeu.java
+++ LightOff_DUSSART_version_console/src/lightoff_dussart_version_console/GrilleDeJeu.java
@@ -3,9 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package lightoff_dussart_version_console;
+import java.util.Random;
 
 /**
- *
+ *Permet de générer, activer, éteindre et afficher l'état des cellules de manière structurée
  * @author mario
  */
 public class GrilleDeJeu {
@@ -14,39 +15,157 @@ public class GrilleDeJeu {
     int nbColonnes;
 
     /**
-     *Constructeur pour créer une grille de jeu avec le nombre
-     * @param p_nbLignes
-     * @param p_nbColonnes
+     * Permet de créer une nouvelle grille de cellules
+     *
+     * @param p_nbLignes     Nombre de lignes de la grille.
+     * @param p_nbColonnes   Nombre de colonnes de la grille.
      */
     public GrilleDeJeu(int p_nbLignes, int p_nbColonnes) {
         nbLignes = p_nbLignes;
         nbColonnes = p_nbColonnes;
         matriceCellules = new CelluleLumineuse[nbLignes][nbColonnes];
-            for (int i = 0; i < nbLignes;
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                matriceCellules[i][j] = new CelluleLumineuse();
+            }
+        }
+    }
 
     /**
-     *
+     * Éteint toutes les cellules de la grille.
      */
-    public void eteindreToutesLesCellules(){
+    public void eteindreToutesLesCellules() {
         for (int i = 0; i < nbLignes; i++) {
             for (int j = 0; j < nbColonnes; j++) {
                 matriceCellules[i][j].eteindreCellule();
             }
         }
-    }
-    
-    public void activerLigneColonneOuDiagonaleAleatoire(){
+    } 
+  
+    /**
+     * Active de manière aléatoire une ligne, une colonne ou une diagonale de cellules.
+     */
+    public void activerLigneColonneOuDiagonaleAleatoire() {
+        Random random = new Random();
+        int choice = random.nextInt(3); // Generates a random number between 0 and 2
         
+        switch (choice) {
+            case 0:
+                int randomRow = random.nextInt(nbLignes);
+                activerLigneDeCellules(randomRow);
+                break;
+            case 1:
+                int randomCol = random.nextInt(nbColonnes);
+                activerColonneDeCellules(randomCol);
+                break;
+            case 2:
+                if (random.nextBoolean()) {
+                    activerDiagonaleDescendante();
+                } else {
+                    activerDiagonaleMontante();
+                }
+                break;
+            default:
+                break;
+        }
     }
-    
+     
+    /**
+     * Génére un plateau de cellules lumineuses de manière aléatoire à partir d’un nombre spécifié de tours
+     * @param nbTours le nombre de tours 
+     */
     public void melangerMatriceAleatoirement(int nbTours){
         this.eteindreToutesLesCellules();
-        for (int i=0; i<nbTours; i++) activerLigneColonneOuDiagonaleAleatoire
+        for (int i=0; i<nbTours; i++) activerLigneColonneOuDiagonaleAleatoire();
     }
     
-    public void activerLigneDeCellule(int idLigne) {
-        for (int i=0; i< nbColonnes; i++){
-            grille[idLigne][i].activerCellule();
+    /**
+    * Change l'état de toutes les cellules de la ligne
+    * @param idLigne numéro de la ligne à changer
+    */
+    public void activerLigneDeCellules(int idLigne) {
+        for (int colonne = 0; colonne < nbColonnes; colonne++) {
+            matriceCellules[idLigne][colonne].activerCellule();
         }
+    }
+
+    /**
+     * Change l'état de toutes les cellules de la colonne
+    * @param idColonne numéro de la colonne à changer.
+    */
+    public void activerColonneDeCellules(int idColonne) {
+        for (int ligne = 0; ligne < nbLignes; ligne++) {
+            matriceCellules[ligne][idColonne].activerCellule();
+        }
+    }
+    
+    
+    /**
+    * Active la diagonale descendante de la grille en allumant les cellules correspondantes.
+    */
+    public void activerDiagonaleDescendante() {
+        for (int i = 0; i < Math.min(nbLignes, nbColonnes); i++) {
+            matriceCellules[i][i].activerCellule();
+        }
+    }
+
+    /**
+    * Active la diagonale montante de la grille en allumant les cellules correspondantes.
+    */
+    public void activerDiagonaleMontante() {
+        for (int i = 0; i < Math.min(nbLignes, nbColonnes); i++) {
+            matriceCellules[i][nbColonnes - 1 - i].activerCellule();
+        }
+    }
+    
+    /**
+    * Vérifie si toutes les cellules de la grille sont éteintes.
+    *
+    * @return "true" si elles sont toutes éteintes, sinon "false".
+    */
+    public boolean cellulesToutesEteintes() {
+        for (int ligne = 0; ligne < nbLignes; ligne++) {
+            for (int colonne = 0; colonne < nbColonnes; colonne++) {
+                if (matriceCellules[ligne][colonne].estEteint()) {
+                    return true;
+                }
+            }
+        }
+        return false; 
+    }
+
+    /**
+    * Génère une représentation visuelle organisée de la grille 
+    * les caractères 'X' et 'O' pour représenter l'état des cellules.
+    *
+    * @return Une chaîne de caractères représentant l'état de la grille.
+    */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("  |");
+        for (int i = 0; i < nbColonnes; i++) {
+            sb.append(" " + i + " |");
+        }
+        sb.append("\n");
+        sb.append("--|");
+        for (int i = 0; i < nbColonnes; i++) {
+            sb.append("---|");
+        }
+        sb.append("\n");
+        for (int i = 0; i < nbLignes; i++) {
+            sb.append(i + " |");
+            for (int j = 0; j < nbColonnes; j++) {
+                sb.append(" " + (matriceCellules[i][j].getEtat() ? "X" : "O") + " |");
+            }
+            sb.append("\n");
+            sb.append("--|");
+            for (int j = 0; j < nbColonnes; j++) {
+                sb.append("---|");
+            }
+            sb.append("\n");
+        }
+    
+        return sb.toString();
     }
 }
--- LightOff_DUSSART_version_console/src/lightoff_dussart_version_console/LightOff_DUSSART_version_console.java
+++ LightOff_DUSSART_version_console/src/lightoff_dussart_version_console/LightOff_DUSSART_version_console.java
@@ -25,6 +25,26 @@ public class LightOff_DUSSART_version_console {
         
         cellule2.getEtat();
         System.out.println("Etat de la cellule est: "+ cellule2);
+        
+        int nbLignes = 10;
+        int nbColonnes = 10;
+                
+        GrilleDeJeu grille = new GrilleDeJeu(nbLignes, nbColonnes);
+        System.out.println(grille);
+        grille.activerLigneDeCellules(1); // Active la première ligne
+        System.out.println(grille); // Vérifiez que la ligne est activée
+        grille.activerColonneDeCellules(2); // Active la deuxième colonne
+        System.out.println(grille); // Vérifiez que la colonne est activée
+        grille.activerDiagonaleDescendante(); // Active la diagonale descendante
+        System.out.println(grille); // Vérifiez que la diagonale est activée
+        grille.activerDiagonaleMontante(); // Active la diagonale montante
+        System.out.println(grille); // Vérifiez que la diagonale montante est activée
+        grille.activerLigneColonneOuDiagonaleAleatoire();
+        System.out.println(grille); // Vérifiez que l'une des lignes, colonnes ou diagonales est activée de manière aléatoire
+          
     }
+    
+    
+    
     
 }
