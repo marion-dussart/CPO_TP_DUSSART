e1dbab2ec8c07970c0f56fba0ece2751bc444010
marion dussart <mario@Ordinateur-Marion.education.lakanal.epf.fr>
2023-10-26 17:14:57 +0200

    fini

--- Exo2/src/exo2/Exo2.java
+++ Exo2/src/exo2/Exo2.java
@@ -4,6 +4,8 @@
  */
 package exo2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author mario
@@ -14,24 +16,30 @@ public class Exo2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    public static void main(String[] args) {
-//Declaration des variables
-int nb; // nombre dentiers a additionner
-int result; // resultat
-int ind; //indice
-nb=5;
-result=0;
-// Addition des nb premiers entiers
-ind=1;
-while (ind <= nb) {
-result=result+ind;
+       //Declaration des variables
+        int nb; // nombre dentiers a additionner
+        int result; // resultat
+        int ind; //indice
+       
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Entrer le nombre :");
+        nb=sc.nextInt(); // On demande a sc de donner le prochain entier
+
+        
+        result=0;
+
+        // Addition des nb premiers entiers
+        ind=1;
+
+        while (ind >= nb) {
+            result=result+ind;
+        }
+
+        // Affichage du resultat
+        System.out.println();
+        System.out.println("La somme des "+ nb + "entiers est: "+result);
+        
+   
 }
-// Affichage du resultat
-System.out.println();
-System.out.println("La somme des "+ nb + "entiers est: "+result);
-    
-    
-    }
-    
 }
+
